ccf91ace39cb0b8fa1c5008581f16b43e5ca887b
Alexander Fonseca <alexander.afonsecac@gmail.com>
2022-03-07 06:02:45 -0500

    configure global routing with Auth0 routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,9 @@
-import { Route } from '@angular/router';
+import {Route} from '@angular/router';
 
-import { AuthGuard as Auth0Guard } from '@auth0/auth0-angular';
+import {AuthGuard as Auth0Guard} from '@auth0/auth0-angular';
 
-import { LayoutComponent } from 'app/layout/layout.component';
-import { InitialDataResolver } from 'app/app.resolvers';
+import {LayoutComponent} from 'app/layout/layout.component';
+import {InitialDataResolver} from 'app/app.resolvers';
 
 // @formatter:off
 /* eslint-disable max-len */
@@ -11,37 +11,41 @@ import { InitialDataResolver } from 'app/app.resolvers';
 export const appRoutes: Route[] = [
 
     // Redirect empty path to '/example'
-    {path: '', pathMatch : 'full', redirectTo: 'example'},
-
-    // Redirect signed in user to the '/example'
-    //
-    // After the user signs in, the sign in page will redirect the user to the 'signed-in-redirect'
-    // path. Below is another redirection for that path to redirect the user to the desired
-    // location. This is a small convenience to keep all main routes together here on this file.
-    {path: 'signed-in-redirect', pathMatch : 'full', redirectTo: 'example'},
+    {path: '', pathMatch: 'full', redirectTo: 'movies'},
+    {path: 'signed-in-redirect', pathMatch: 'full', redirectTo: 'movies'},
     // Landing routes
     {
         path: '',
-        component  : LayoutComponent,
+        component: LayoutComponent,
         data: {
             layout: 'empty'
         },
-        children   : [
-            {path: 'home', loadChildren: () => import('app/modules/landing/home/home.module').then(m => m.LandingHomeModule)},
+        children: [
+            {
+                path: 'home',
+                loadChildren: () => import('app/modules/landing/home/home.module').then(m => m.LandingHomeModule)
+            },
         ]
     },
 
     // Admin routes
     {
-        path       : '',
+        path: '',
         canActivate: [Auth0Guard],
         canActivateChild: [Auth0Guard],
-        component  : LayoutComponent,
-        resolve    : {
+        component: LayoutComponent,
+        resolve: {
             initialData: InitialDataResolver,
         },
-        children   : [
-            {path: 'example', loadChildren: () => import('app/modules/admin/example/example.module').then(m => m.ExampleModule)},
+        children: [
+            {
+                path: 'example',
+                loadChildren: () => import('app/modules/admin/example/example.module').then(m => m.ExampleModule)
+            },
+            {
+                path: 'movies',
+                loadChildren: () => import('app/modules/admin/movies/movie.module').then(m => m.MovieModule)
+            }
         ]
     }
 ];
