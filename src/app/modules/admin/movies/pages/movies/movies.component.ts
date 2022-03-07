610eaf75f8a4f1c6213f76e3bc5a6c9066634d8e
Alexander Fonseca <alexander.afonsecac@gmail.com>
2022-03-07 08:59:11 -0500

    edit: movie list page * complete filter movies * complete order movies by title or year (asc, desc)

--- src/app/modules/admin/movies/pages/movies/movies.component.ts
+++ src/app/modules/admin/movies/pages/movies/movies.component.ts
@@ -6,8 +6,10 @@ import {
     OnInit,
     ViewEncapsulation
 } from '@angular/core';
+import {MatSelectChange} from '@angular/material/select';
 
-import {BehaviorSubject, Subject, takeUntil} from 'rxjs';
+import {BehaviorSubject, combineLatest, Subject, takeUntil} from 'rxjs';
+
 import {MovieService} from '../../service/movie.service';
 
 @Component({
@@ -21,11 +23,16 @@ export class MoviesComponent implements OnInit, OnDestroy {
 
     movies: any[];
     filteredMovies: any[];
+    categories: string[] = ['movie', 'series'];
+    ascOrDesc: boolean = true;
+    sortKey: string = 'title';
 
     filters: {
         query$: BehaviorSubject<string>;
+        category$: BehaviorSubject<string>;
     } = {
         query$: new BehaviorSubject(''),
+        category$: new BehaviorSubject('all'),
     };
 
     private _unsubscribeAll: Subject<any> = new Subject<any>();
@@ -42,19 +49,37 @@ export class MoviesComponent implements OnInit, OnDestroy {
             .pipe((takeUntil(this._unsubscribeAll)))
             .subscribe((movies) => {
                 this.movies = this.filteredMovies = movies;
+                this.sortBy();
                 this._changeDetectorRef.markForCheck();
             });
 
-        this.filters.query$.subscribe((query) => {
+        combineLatest(
+            [
+                this.filters.query$,
+                this.filters.category$
+            ]
+        )
+            .pipe((takeUntil(this._unsubscribeAll)))
+            .subscribe(([query, category]) => {
+                this.filteredMovies = this.movies;
 
-            this.filteredMovies = this.movies;
+                if (query?.length > 0) {
+                    const queryLower = query.toLowerCase();
+                    this.filteredMovies = this.filteredMovies?.filter(
+                        (movie: any) => movie.title.toString().toLowerCase().indexOf(queryLower) >= 0 ||
+                            movie.description.toString().toLowerCase().indexOf(queryLower) >= 0 ||
+                            movie.releaseYear.toString().includes(queryLower)
+                    );
+                }
 
-            if (query !== '') {
-                this.filteredMovies = this.filteredMovies.filter(movie => movie.title.toLowerCase().includes(query.toLowerCase())
-                    || movie.description.toLowerCase().includes(query.toLowerCase())
-                    || movie.category.toLowerCase().includes(query.toLowerCase()));
-            }
-        });
+                if (category !== 'all') {
+                    this.filteredMovies = this.filteredMovies.filter(movie => movie.programType === category);
+                }
+
+                this.sortBy();
+
+                this._changeDetectorRef.markForCheck();
+            });
     }
 
     ngOnDestroy(): void {
@@ -62,11 +87,31 @@ export class MoviesComponent implements OnInit, OnDestroy {
         this._unsubscribeAll.complete();
     }
 
+    filterByCategory(change: MatSelectChange): void {
+        this.filters.category$.next(change.value);
+    }
+
     filterByQuery(query: string): void {
         this.filters.query$.next(query);
     }
 
+    changeOrderBy(change: MatSelectChange): void {
+        this.sortKey = change.value;
+        this.sortBy();
+    }
+
+    sortBy(): void {
+        const key = this.sortKey;
+        this.filteredMovies = this.filteredMovies.sort((a: any, b: any) => {
+            if (this.ascOrDesc) {
+                return a[key].toString() > b[key].toString() ? 1 : b[key].toString() > a[key].toString() ? -1 : 0;
+            } else {
+                return a[key].toString() < b[key].toString() ? 1 : b[key].toString() < a[key].toString() ? -1 : 0;
+            }
+        });
+    }
+
     trackByFn(index: number, item: any): any {
-        return item.id || index;
+        return item.title || index;
     }
 }
